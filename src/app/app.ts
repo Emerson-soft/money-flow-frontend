795156772ebe20a190eaacfdff3198c4a89252fe
Emerson Mendonça <emerson_10_s@hotmail.com>
2026-08-03 19:50:46 -0300

    feat - adiciona dcoker file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [HttpClient],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  protected readonly title = signal('frontend teste');
+
+  constructor(
+    private readonly _http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this._http.get("http://localhost:8080/weatherforecast").subscribe({
+      next: (data) => {
+        console.log(data)
+      }
+    })
+  }
 }
